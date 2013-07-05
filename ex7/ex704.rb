9b44bcbebd10258c02eb00dcc59f1d6028e842cc
tomoyuki@sakura <tomoyuki@www1269ue.sakura.ne.jp>
2013-07-06 04:22:36 +0900

    p 107

---
+++ ex7/ex704.rb
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+class Student
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+
+  def name
+    @name
+  end
+
+  def age
+    @age
+  end
+
+  attr_accessor :name, :age
+
+  def to_s
+    "#@name, #@age"
+  end
+end
+
+shin = Student.new('久保秋　真', 45)
+
+puts "氏名：#{shin.name}, 年齢：#{shin.age}歳"
+
+shin.name = 'Singh, Tiger Jeet'
+shin.age = 445
+
+puts shin.name
+puts shin.age
+
+puts "氏名：#{shin.name}, 年齢：#{shin.age}歳"
+
